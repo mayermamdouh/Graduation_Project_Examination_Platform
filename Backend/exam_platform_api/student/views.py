cbecdcfabb028aeac63452b7928418585eb65dba
Mahmoud Elhawawshy <M-Elhawawshy@github.com>
2024-04-18 13:31:55 +0200

    added basic backend functionality

---
+++ Backend/exam_platform_api/student/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse, JsonResponse
+import json
+
+def index(request):
+
+    data = {
+        'name': 'mahmoud',
+    }
+
+    jason_data = json.dumps(data)
+
+    return JsonResponse(jason_data, safe=False)
