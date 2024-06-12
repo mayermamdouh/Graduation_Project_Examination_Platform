41d7727f236b2dd4d3667b39218ea9f10a8614f2
Mahmoud <ma774104@gmail.com>
2024-06-12 23:46:02 +0300

    added custom permission message

--- Backend/exam_platform_api/group/permissions.py
+++ Backend/exam_platform_api/group/permissions.py
@@ -6,7 +6,7 @@ class IsGroupOwner(BasePermission):
     """
     Custom permission to check if the requesting user is the owner of the group.
     """
-    message = 'You do not have permission to perform this action or the group does not exist'
+    message = 'You do not have permission to perform this action, You are not the group instructor.'
 
     def has_permission(self, request, view):
         # Retrieve the group ID from the URL (assuming it's provided as a URL parameter)
@@ -16,6 +16,7 @@ class IsGroupOwner(BasePermission):
         try:
             group = Group.objects.get(pk=group_id)
         except Group.DoesNotExist:
+            self.message = "Group does not exist"
             return False
 
         # Check if the requesting user is the owner of the group
