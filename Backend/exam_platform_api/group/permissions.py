1135deae5eb37c2a526ddbac07897530a7f5fc08
Mahmoud <ma774104@gmail.com>
2024-06-13 00:01:59 +0300

    fixed permission bug

--- Backend/exam_platform_api/group/permissions.py
+++ Backend/exam_platform_api/group/permissions.py
@@ -1,4 +1,6 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+
 from .models import Group
 
 
@@ -8,14 +10,28 @@ class IsGroupOwner(BasePermission):
     """
     message = 'You do not have permission to perform this action, You are not the group instructor.'
 
-    def has_permission(self, request, view):
-        # Retrieve the group ID from the URL (assuming it's provided as a URL parameter)
-        group_id = view.kwargs.get('pk')
+    def has_permission(self, request: Request, view):
+        group_id = request.data.get('pk') or request.query_params.get('pk')
+        group_code = request.data.get('group_code') or request.query_params.get('group_code')
+        group = None
 
-        # Retrieve the group object based on the group ID
-        try:
-            group = Group.objects.get(pk=group_id)
-        except Group.DoesNotExist:
+        # Attempt to retrieve the group by ID
+        if group_id is not None:
+            try:
+                group = Group.objects.get(pk=group_id)
+            except Group.DoesNotExist:
+                pass
+
+        # If the group was not found by ID, attempt to retrieve by group code
+        if group is None and group_code is not None:
+            try:
+                group = Group.objects.get(code=group_code)
+            except Group.DoesNotExist:
+                self.message = "Group does not exist"
+                return False
+
+        # If the group is still None, it means both retrieval attempts failed
+        if group is None:
             self.message = "Group does not exist"
             return False
 
